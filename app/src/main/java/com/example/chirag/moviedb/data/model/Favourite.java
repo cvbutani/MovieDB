b00b977139bd44e4d9f57c8b79ab27fb3738c3b7
Chirag Butani <chirag.butani1@gmail.com>
2018-11-09 08:12:43 -0500

    favourite movie and TV displayed

--- app/src/main/java/com/example/chirag/moviedb/data/model/Favourite.java
+++ app/src/main/java/com/example/chirag/moviedb/data/model/Favourite.java
@@ -12,74 +12,88 @@ import android.support.annotation.NonNull;
 @Entity(tableName = "favourite")
 public class Favourite {
 
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "primary_key")
-    private Integer primary_key;
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "primary_key")
+        private Integer primary_key;
 
-    @NonNull
-    @ColumnInfo(name = "id")
-    private Integer id;
+        @NonNull
+        @ColumnInfo(name = "id")
+        private Integer id;
 
-    @NonNull
-    @ColumnInfo(name = "email")
-    private String email;
+        @NonNull
+        @ColumnInfo(name = "email")
+        private String email;
 
-    @NonNull
-    @ColumnInfo(name = "poster")
-    private String poster;
+        @NonNull
+        @ColumnInfo(name = "poster")
+        private String poster;
 
-    @NonNull
-    @ColumnInfo(name = "title")
-    private String title;
+        @NonNull
+        @ColumnInfo(name = "title")
+        private String title;
 
-    public Favourite(@NonNull Integer id, @NonNull String email, @NonNull String poster, @NonNull String title) {
-        this.id = id;
-        this.email = email;
-        this.poster = poster;
-        this.title = title;
-    }
+        @NonNull
+        @ColumnInfo(name = "content_type")
+        private String content_type;
 
-    public Integer getPrimary_key() {
-        return primary_key;
-    }
+        public Favourite(@NonNull Integer id, @NonNull String email, @NonNull String poster, @NonNull String title, @NonNull String content_type ) {
+                this.id = id;
+                this.email = email;
+                this.poster = poster;
+                this.title = title;
+                this.content_type = content_type;
+        }
 
-    public void setPrimary_key(Integer primary_key) {
-        this.primary_key = primary_key;
-    }
+        public Integer getPrimary_key() {
+                return primary_key;
+        }
 
-    @NonNull
-    public Integer getId() {
-        return id;
-    }
+        public void setPrimary_key(Integer primary_key) {
+                this.primary_key = primary_key;
+        }
 
-    public void setId(@NonNull Integer id) {
-        this.id = id;
-    }
+        @NonNull
+        public Integer getId() {
+                return id;
+        }
 
-    @NonNull
-    public String getEmail() {
-        return email;
-    }
+        public void setId(@NonNull Integer id) {
+                this.id = id;
+        }
 
-    public void setEmail(@NonNull String email) {
-        this.email = email;
-    }
+        @NonNull
+        public String getEmail() {
+                return email;
+        }
 
-    @NonNull
-    public String getPoster() {
-        return poster;
-    }
+        public void setEmail(@NonNull String email) {
+                this.email = email;
+        }
 
-    public void setPoster(@NonNull String poster) {
-        this.poster = poster;
-    }
+        @NonNull
+        public String getPoster() {
+                return poster;
+        }
 
-    @NonNull
-    public String getTitle() {
-        return title;
-    }
+        public void setPoster(@NonNull String poster) {
+                this.poster = poster;
+        }
 
-    public void setTitle(@NonNull String title) {
-        this.title = title;
-    }
+        @NonNull
+        public String getTitle() {
+                return title;
+        }
+
+        public void setTitle(@NonNull String title) {
+                this.title = title;
+        }
+
+        @NonNull
+        public String getContent_type() {
+                return content_type;
+        }
+
+        public void setContent_type(@NonNull String content_type) {
+                this.content_type = content_type;
+        }
 }
--- app/src/main/java/com/example/chirag/moviedb/dbtv/DbTvFragment.java
+++ app/src/main/java/com/example/chirag/moviedb/dbtv/DbTvFragment.java
@@ -37,142 +37,149 @@ import static com.example.chirag.moviedb.data.Constant.EXTRA_TITLE;
  */
 public class DbTvFragment extends Fragment implements DBTvContract.View {
 
-    CardView nowPlayingCardView;
-    CardView upcomingCardView;
-    CardView popularCardView;
-    CardView topRatedCardView;
+        CardView nowPlayingCardView;
+        CardView upcomingCardView;
+        CardView popularCardView;
+        CardView topRatedCardView;
 
-    RelativeLayout mNoInternet;
+        RelativeLayout mNoInternet;
 
-    private LinearLayout mLinearLayoutMovieHome;
-    private LinearLayout mLinearLayoutNowPlaying;
-    private LinearLayout mLinearLayoutTopRated;
-    private LinearLayout mLinearLayoutUpcoming;
+        private LinearLayout mLinearLayoutMovieHome;
+        private LinearLayout mLinearLayoutNowPlaying;
+        private LinearLayout mLinearLayoutTopRated;
+        private LinearLayout mLinearLayoutUpcoming;
 
-    TextView mTextViewTopRated;
-    TextView mTextViewPopular;
-    TextView mTextViewLatest;
+        TextView mTextViewTopRated;
+        TextView mTextViewPopular;
+        TextView mTextViewLatest;
 
-    private ConnectivityManager mConnectivityManager;
-    private NetworkInfo mActiveNetwork;
+        private ConnectivityManager mConnectivityManager;
+        private NetworkInfo mActiveNetwork;
 
-    private static final String POSTER_IMAGE_URL = "http://image.tmdb.org/t/p/w185/";
+        private static final String POSTER_IMAGE_URL = "http://image.tmdb.org/t/p/w185/";
 
-    int mMovieId;
-    String mUserEmail;
-    public DbTvFragment() {
-    }
+        int mMovieId;
+        String mUserEmail;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (!getArguments().isEmpty()) {
-            mUserEmail = getArguments().getString("EXTRA_EMAIL");
+        public DbTvFragment() {
         }
 
-        DBTvPresenter tvPresenter = new DBTvPresenter(getContext(), checkInternetConnection());
-        tvPresenter.attachView(this);
-    }
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.movie_home, container, false);
-        mLinearLayoutMovieHome = rootView.findViewById(R.id.movie_popular);
-        mLinearLayoutNowPlaying = rootView.findViewById(R.id.movie_now_playing);
-        nowPlayingCardView = rootView.findViewById(R.id.now_playing_card);
-        nowPlayingCardView.setVisibility(View.GONE);
-        upcomingCardView = rootView.findViewById(R.id.upcoming_card);
-        upcomingCardView.setVisibility(View.GONE);
-        topRatedCardView = rootView.findViewById(R.id.top_rated_card);
-        popularCardView = rootView.findViewById(R.id.popular_card);
-        mLinearLayoutTopRated = rootView.findViewById(R.id.movie_top_rated);
-        mLinearLayoutUpcoming = rootView.findViewById(R.id.movie_upcoming);
-        mTextViewTopRated = rootView.findViewById(R.id.movie_top_rated_label);
-        mTextViewPopular = rootView.findViewById(R.id.popular_label);
-        mTextViewLatest = rootView.findViewById(R.id.now_playing_label);
-        mNoInternet = rootView.findViewById(R.id.no_internet);
-        if (checkInternetConnection()) {
-            popularCardView.setVisibility(View.VISIBLE);
-            topRatedCardView.setVisibility(View.VISIBLE);
-            mNoInternet.setVisibility(View.GONE);
-        } else {
-            popularCardView.setVisibility(View.GONE);
-            upcomingCardView.setVisibility(View.GONE);
-            topRatedCardView.setVisibility(View.GONE);
-            nowPlayingCardView.setVisibility(View.GONE);
-            mNoInternet.setVisibility(View.VISIBLE);
-        }
-        return rootView;
-    }
-
-    @Override
-    public void getPopularTvHome(Result data) {
-        setLayout(data, mLinearLayoutMovieHome, mTextViewPopular, "Popular TV Shows");
-    }
-
-    @Override
-    public void getResultFailure(String errorMessage) {
-
-    }
-
-    @Override
-    public void getTopRatedTvHome(Result data) {
-        setLayout(data, mLinearLayoutTopRated, mTextViewTopRated, "Top Rated TV Shows");
-    }
-
-    private void startNewActivity(int movieId, String name) {
-        Intent intent = new Intent(getContext(), MovieDetailActivity.class);
-        intent.putExtra(EXTRA_ID, movieId);
-        intent.putExtra(EXTRA_TITLE, name);
-        intent.putExtra("EXTRA_EMAIL", mUserEmail);
-        intent.putExtra(CONTENT_TYPE, CONTENT_TV);
-        startActivity(intent);
-    }
-
-    private void setLayout(Result data, LinearLayout layout, TextView textView, String title) {
-        if (checkInternetConnection()) {
-            if (data != null && !data.getResults().isEmpty()) {
-                popularCardView.setVisibility(View.VISIBLE);
-                topRatedCardView.setVisibility(View.VISIBLE);
-                layout.removeAllViews();
-                textView.setText(title);
-                for (final ResultResponse item : data.getResults()) {
-                    View parent = getLayoutInflater().inflate(R.layout.movie_home_poster, layout, false);
-                    ImageView poster = parent.findViewById(R.id.movie_home_imageview);
-                    StringBuilder builder = new StringBuilder();
-                    String imagePosterString = builder.append(POSTER_IMAGE_URL).append(item.getPoster()).toString();
-                    Picasso.get().load(imagePosterString).into(poster);
-
-                    poster.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            mMovieId = item.getId();
-                            String movieName = item.getName();
-                            startNewActivity(mMovieId, movieName);
-                        }
-                    });
-                    layout.addView(parent);
+                if (!getArguments().isEmpty()) {
+                        mUserEmail = getArguments().getString("EXTRA_EMAIL");
                 }
-            }
-        } else {
-            popularCardView.setVisibility(View.GONE);
-            upcomingCardView.setVisibility(View.GONE);
-            topRatedCardView.setVisibility(View.GONE);
-            nowPlayingCardView.setVisibility(View.GONE);
-        }
-    }
 
-    private boolean checkInternetConnection() {
-
-        if (getActivity() != null) {
-            mConnectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        }
-        if (mConnectivityManager != null) {
-            mActiveNetwork = mConnectivityManager.getActiveNetworkInfo();
+                DBTvPresenter tvPresenter = new DBTvPresenter(getContext(), checkInternetConnection());
+                tvPresenter.attachView(this);
         }
 
-        return (mActiveNetwork != null) && (mActiveNetwork.isConnectedOrConnecting());
-    }
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+                View rootView = inflater.inflate(R.layout.movie_home, container, false);
+                mLinearLayoutMovieHome = rootView.findViewById(R.id.movie_popular);
+                mLinearLayoutNowPlaying = rootView.findViewById(R.id.movie_now_playing);
+                mLinearLayoutTopRated = rootView.findViewById(R.id.movie_top_rated);
+                mLinearLayoutUpcoming = rootView.findViewById(R.id.movie_upcoming);
+
+                nowPlayingCardView = rootView.findViewById(R.id.now_playing_card);
+                nowPlayingCardView.setVisibility(View.GONE);
+
+                upcomingCardView = rootView.findViewById(R.id.upcoming_card);
+                upcomingCardView.setVisibility(View.GONE);
+
+                topRatedCardView = rootView.findViewById(R.id.top_rated_card);
+
+                popularCardView = rootView.findViewById(R.id.popular_card);
+
+                mTextViewTopRated = rootView.findViewById(R.id.movie_top_rated_label);
+                mTextViewPopular = rootView.findViewById(R.id.popular_label);
+                mTextViewLatest = rootView.findViewById(R.id.now_playing_label);
+
+                mNoInternet = rootView.findViewById(R.id.no_internet);
+                if (checkInternetConnection()) {
+                        popularCardView.setVisibility(View.VISIBLE);
+                        topRatedCardView.setVisibility(View.VISIBLE);
+                        mNoInternet.setVisibility(View.GONE);
+                } else {
+                        popularCardView.setVisibility(View.GONE);
+                        upcomingCardView.setVisibility(View.GONE);
+                        topRatedCardView.setVisibility(View.GONE);
+                        nowPlayingCardView.setVisibility(View.GONE);
+                        mNoInternet.setVisibility(View.VISIBLE);
+                }
+                return rootView;
+        }
+
+        @Override
+        public void getPopularTvHome(Result data) {
+                setLayout(data, mLinearLayoutMovieHome, mTextViewPopular, "Popular TV Shows");
+        }
+
+        @Override
+        public void getResultFailure(String errorMessage) {
+
+        }
+
+        @Override
+        public void getTopRatedTvHome(Result data) {
+                setLayout(data, mLinearLayoutTopRated, mTextViewTopRated, "Top Rated TV Shows");
+        }
+
+        private void startNewActivity(int movieId, String name) {
+                Intent intent = new Intent(getContext(), MovieDetailActivity.class);
+                intent.putExtra(EXTRA_ID, movieId);
+                intent.putExtra(EXTRA_TITLE, name);
+                intent.putExtra("EXTRA_EMAIL", mUserEmail);
+                intent.putExtra(CONTENT_TYPE, CONTENT_TV);
+                startActivity(intent);
+        }
+
+        private void setLayout(Result data, LinearLayout layout, TextView textView, String title) {
+                if (checkInternetConnection()) {
+                        if (data != null && !data.getResults().isEmpty()) {
+                                popularCardView.setVisibility(View.VISIBLE);
+                                topRatedCardView.setVisibility(View.VISIBLE);
+                                layout.removeAllViews();
+                                textView.setText(title);
+                                for (final ResultResponse item : data.getResults()) {
+                                        View parent = getLayoutInflater().inflate(R.layout.movie_home_poster, layout, false);
+                                        ImageView poster = parent.findViewById(R.id.movie_home_imageview);
+                                        StringBuilder builder = new StringBuilder();
+                                        String imagePosterString = builder.append(POSTER_IMAGE_URL).append(item.getPoster()).toString();
+                                        Picasso.get().load(imagePosterString).into(poster);
+
+                                        poster.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View view) {
+                                                        mMovieId = item.getId();
+                                                        String movieName = item.getName();
+                                                        startNewActivity(mMovieId, movieName);
+                                                }
+                                        });
+                                        layout.addView(parent);
+                                }
+                        }
+                } else {
+                        popularCardView.setVisibility(View.GONE);
+                        upcomingCardView.setVisibility(View.GONE);
+                        topRatedCardView.setVisibility(View.GONE);
+                        nowPlayingCardView.setVisibility(View.GONE);
+                }
+        }
+
+        private boolean checkInternetConnection() {
+
+                if (getActivity() != null) {
+                        mConnectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+                }
+                if (mConnectivityManager != null) {
+                        mActiveNetwork = mConnectivityManager.getActiveNetworkInfo();
+                }
+
+                return (mActiveNetwork != null) && (mActiveNetwork.isConnectedOrConnecting());
+        }
 }
